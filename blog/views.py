9e7956cd19e5c591736808f1da632856ef547d1d
bhx98 <baharahfallahi@gmail.com>
2023-06-16 11:53:54 +0330

    Add paginator library to view

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from . import serializers
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 class IndexPage(TemplateView):
