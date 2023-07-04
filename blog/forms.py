35af5acb869c6cf63ea5bc5a88d63c59818e851c
Bahareh Fallahi <baharahfallahi@gmail.com>
2023-07-04 13:18:30 +0330

    Override the user creation form

--- blog/forms.py
+++ blog/forms.py
@@ -9,3 +9,26 @@ class LoginForm(forms.Form):
     email = forms.EmailField(widget=forms.EmailInput(
         attrs={'placeholder': 'Email', 'style': 'width: 300px;'}))
 
+# Override the user creation form
+
+
+class UserCreateForm(UserCreationForm):
+    email = forms.EmailField(
+        required=True, label='Email', error_messages={'exitst': 'This already exists!'})
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+    def save(self, commit=True):
+        user = super(UserCreationForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
+
+    def clean_email(self):
+        if User.objects.filter(email=self.cleaned_data['email']).exists():
+            raise forms.ValidationError(
+                self.fields['email'].error_messages['exists'])
+        return self.cleaned_data['email']
