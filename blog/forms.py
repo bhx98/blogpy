a0e6e15fbcf3a618cd7d660165f8c6eae5233a60
Bahareh Fallahi <baharahfallahi@gmail.com>
2023-07-04 13:22:11 +0330

    add equilivent models to forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Comment, ContactUsMessage
 from django.core.validators import EmailValidator
 from ckeditor.fields import RichTextField
 from django.forms import TextInput, EmailInput
