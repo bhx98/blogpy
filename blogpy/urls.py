233daf6e80b3d183b47035b9bb218a7c99e04063
Bahareh Fallahi <baharahfallahi@gmail.com>
2023-07-04 13:30:28 +0330

    add urls for accounts based on django.contrib.auth

--- blogpy/urls.py
+++ blogpy/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('', include('blog.urls')),
+    path("accounts/", include("django.contrib.auth.urls")),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('tinymce/', include('tinymce.urls')),
