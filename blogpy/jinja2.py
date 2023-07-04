7ba5dd7c7077dbef871b7c89256293490216ef67
Bahareh Fallahi <baharahfallahi@gmail.com>
2023-07-04 11:38:31 +0330

    add django_browser_reload_script in jinja2

--- blogpy/jinja2.py
+++ blogpy/jinja2.py
@@ -1,5 +1,7 @@
 from jinja2 import Environment
 from django.urls import reverse
+from django_browser_reload.jinja import django_browser_reload_script
+
 from django.contrib.staticfiles.storage import staticfiles_storage
 # for more later django installations use:
 # from django.templatetags.static import static
@@ -9,6 +11,8 @@ def environment(**options):
     env = Environment(**options)
     env.globals.update({
         "static": staticfiles_storage.url,
-        "url": reverse
+        "url": reverse,
+        "django_browser_reload_script": django_browser_reload_script,
+
     })
     return env
