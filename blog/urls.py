4b4b8c777d1e9a03c26e6c4aaa711a05db980a21
Bahareh Fallahi <baharahfallahi@gmail.com>
2023-07-04 13:37:18 +0330

    add / to specific url

--- blog/urls.py
+++ blog/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
          name='single_article'),
     path('article/search/', views.SearchArticleAPIView.as_view(),
          name='search_article'),
-    path('article/add', views.SubmitArticleAPIView.as_view(),
+    path('article/add/', views.SubmitArticleAPIView.as_view(),
          name='submit_article'),
     path('article/update-cover/',
          views.UpdateArticleAPIView.as_view(), name='update_article'),
