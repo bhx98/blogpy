580ceef76585a4909e88209c73a904310d2e3721
Bahareh Fallahi <baharahfallahi@gmail.com>
2023-07-04 13:19:16 +0330

    add content to Meta class CommentForm

--- blog/forms.py
+++ blog/forms.py
@@ -3,6 +3,8 @@ from .models import Comment
 
 class CommentForm(forms.ModelForm):
     class Meta:
+        model = Comment
+        fields = ('name', 'email', 'body')
 class LoginForm(forms.Form):
     name = forms.CharField(widget=forms.TextInput(
         attrs={'placeholder': 'Name', 'style': 'width: 300px;'}))
