df3a5b6f264bad24c6db3c1f3a977af7e5b69016
Bahareh Fallahi <baharahfallahi@gmail.com>
2023-07-04 11:23:36 +0330

    add verbose_name for cover field in category class

--- blog/models.py
+++ blog/models.py
@@ -73,7 +73,7 @@ class Category(models.Model):
     title = models.CharField(max_length=128, null=False,
                              blank=False, verbose_name="عنوان دسته بندی")
     cover = models.ImageField(
-        upload_to='files/category_cover/', null=False, blank=False, validators=[validate_file_extension])
+        upload_to='files/category_cover/', null=False, blank=False, verbose_name="تصویر")
     # magic method/dunder
 
     def img_preview(self):
